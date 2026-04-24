500108d7915f7bce5203b52a593852b5975f3950
Abhijeet Mishra <abhijitmishra3089@gmail.com>
2026-04-24 11:35:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+const heading=document.createElement("h1");
+heading.id="sizeInfo";
+// heading.innerHTML="<span>Width: x and Height: y</span>";
+document.body.appendChild(heading);
+function updatesize() {
+	heading.innerHTML=`<span>Width: ${window.innerWidth} and Height: ${window.innerHeight}</span>`
+}
+updatesize()
+window.addEventListener("resize",updatesize)
